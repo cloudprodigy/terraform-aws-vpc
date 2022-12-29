7743d22c285ed0664ed27ce05b00834d89aa4d7a
Ricky <ricky@cloudprodigy.ca>
2022-12-29 13:16:54 -0500

    updated doc

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 /**
  * 
  *
- * This module creates following resources:
- * VPC 
- * Public & Private Subnets
- * NAT Gateway 
- * Internet Gateway
- * Network ACLs
- * Routes
- * VPC Endpoints
- * VPC FlowLog
+ * ## This module creates following resources:
+ * - VPC 
+ * - Public & Private Subnets
+ * - NAT Gateway 
+ * - Internet Gateway
+ * - Network ACLs
+ * - Routes
+ * - VPC Endpoints
+ * - VPC FlowLog
+ * - One NAT Gateway per AZ
+ * - Custom NAT Gateways
+ * - Multiple Network ACLs
  */
